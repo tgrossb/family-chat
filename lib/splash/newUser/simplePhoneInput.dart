61935cad9e9803032e668a491d8d792a44d33cc9
Theo Grossberndt <tgrossb87@gmail.com>
2018-08-28 21:43:52 -0400

    More work on the phone mask

--- lib/splash/newUser/simplePhoneInput.dart
+++ lib/splash/newUser/simplePhoneInput.dart
@@ -1,3 +1,4 @@
+import 'dart:collection';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -59,8 +60,8 @@ class SimplePhoneInputState extends State<SimplePhoneInput> with SingleTickerPro
     if (value.length == 0 && widget.isRequired)
       return "Enter your phone number";
 
-    if (value.length != 0 && !numberMatcher.hasMatch(value))
-      return "";
+    if (value.length != 0 && !(NumberTextInputFormatter.stripNumbers(value).length == 10))
+      return "Enter a valid 10-digit phone number";
 
     savePhone(phone);
     return null;
@@ -131,29 +132,117 @@ class SimplePhoneInputState extends State<SimplePhoneInput> with SingleTickerPro
 }
 
 class NumberTextInputFormatter extends TextInputFormatter {
-  static List<int> stripNumbers(String s){
-    return s.codeUnits.where((charCode) => 47 < charCode && charCode < 58).toList();
+  static Queue<int> stripNumbers(String s){
+    return Queue.of(s.codeUnits.where((charCode) => 47 < charCode && charCode < 58));
+  }
+
+  String mask, masker, placeHolder;
+  List<String> maskList;
+  int lastMask;
+  int staticCounter;
+
+  NumberTextInputFormatter({this.mask: "(xxx) xxx - xxxx",
+                            this.masker: "x",
+                            this.placeHolder: " "}){
+    maskList = mask.split("");
+    print(maskList);
+    lastMask = maskList.lastIndexOf(masker);
   }
 
   @override
   TextEditingValue formatEditUpdate(TextEditingValue oldValue, TextEditingValue newValue) {
-    List<int> phoneNums = stripNumbers(newValue.text);
-    int phoneCount = phoneNums.length;
-    List<String> output = ["(", " ", " ", " ", ") ", " ", " ", " ", " - ", " ", " ", " ", " "];
-    int outputIndex = 1;
-    for (int phoneIndex=0; phoneIndex<phoneCount; phoneIndex++){
-      outputIndex = phoneIndex + (phoneIndex > 2 ? (phoneIndex > 5 ? 3 : 2) : 1);
-      output[outputIndex] = String.fromCharCode(phoneNums[phoneIndex]);
+    Queue<int> oldNums = stripNumbers(oldValue.text);
+    Queue<int> phoneNums = stripNumbers(newValue.text);
+    bool adding = oldNums.length < phoneNums.length;
+    bool deleting = oldNums.length > phoneNums.length;
+
+    // 3 static movements in a row is a sign that something is being ignored
+    if (oldNums.length == phoneNums.length)
+      staticCounter++;
+    else
+      staticCounter = 0;
+
+    // If there are 3 or more statics in a row, remove the last character from newValue if there are any
+    // Also, set deleting to true and adding to false if phoneNums is not 0 after this
+    if (staticCounter > 2){
+      print("3 statics");
+      staticCounter = 0;
+      if (phoneNums.length > 0)
+        phoneNums.removeLast();
+      if (phoneNums.length == 0){
+        adding = true;
+        deleting = false;
+      } else {
+        adding = false;
+        deleting = true;
+      }
     }
 
-    // Sum the length of the output up to outputIndex
-    int outputCharIndex = 0;
-    for (int c=0; c<outputIndex; c++)
-      outputCharIndex += output[c].length;
+    StringBuffer output = StringBuffer();
+
+    // Stream through the mask, placing numbers in order at mask characters
+    // When out of numbers, place placeHolder at that mask
+    // Additionally, find the string index of the first place holder
+    int nextMaskStart;
+    int characterCounter = 0;
+
+    print(adding ? "Adding" : deleting ? "Deleting" : "Static");
+
+    for (String maskCharacter in maskList){
+      if (phoneNums.isEmpty) {
+        // This is when the nextMaskStart becomes relevant
+        if (maskCharacter == masker) {
+          // If this is a masker character, just set the selected index to here if it hasn't been set
+//          nextMaskStart ?? print("Setting nextMaskStart to $characterCounter (i1)");
+          nextMaskStart ??= characterCounter;
+        } else if (deleting) {
+          // If numbers are being subtracted, and this character is a non-masker character, set the next mask start to right before this
+//          nextMaskStart ?? print("Setting nextMaskStart to $characterCounter (i2)");
+          nextMaskStart ??= characterCounter;
+        }
+        // If numbers are being added and this character is a non-masker character, wait the next masker character (do nothing)
+      }
+
+      if (maskCharacter == masker && phoneNums.isNotEmpty)
+        // If this is a mask character and there is a phone number, add the number
+        output.writeCharCode(phoneNums.removeFirst());
+      else if (maskCharacter == masker && phoneNums.isEmpty)
+        // If this is a masker character but there are no more numbers, add a place holder
+        output.write(placeHolder);
+      else
+        // If this is not a masker character, copy from the mask
+        output.write(maskCharacter);
+
+      characterCounter++;
+    }
+
+    // If the nextMaskStart is still null, set it to the end
+    nextMaskStart ??= maskList.length;
+
+
+    if (newValue.selection.baseOffset != newValue.text.length){
+      print("Not on");
+      int masks = 0;
+      for (int c=0; c<maskList.length; c++){
+        if (masks == newValue.selection.baseOffset)
+          nextMaskStart = c;
+        if (maskList[c] == masker)
+          masks++;
+      }
+      // If adding, find the next non masker character
+      if (adding)
+        nextMaskStart = maskList.indexOf(masker, nextMaskStart);
+      // If deleting, find the next non masker character backwards
+      else if (deleting)
+        for (; nextMaskStart>1; nextMaskStart--)
+          if (maskList[nextMaskStart-1] == masker)
+            break;
+      print("End: $nextMaskStart ($adding, $deleting)");
+    }
 
     return new TextEditingValue(
-      text: output.join(),
-      selection: new TextSelection.collapsed(offset: outputCharIndex+1),
+      text: output.toString(),
+      selection: new TextSelection.collapsed(offset: nextMaskStart),
     );
   }
 }
--- lib/splash/newUser/validators.dart
+++ lib/splash/newUser/validators.dart
@@ -3,7 +3,7 @@ import 'package:bodt_chat/user.dart';
 class Validators {
   static RegExp nameExp = new RegExp(r'^[A-Za-z ]+$');
   static RegExp emailChecker = new RegExp(r"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?");
-  static RegExp dobExp = new RegExp(r'^(0[1-9]|1[0-2])/([1-9]|[12][0-9]|[3][01])/([2]([0-9])\1{3}|19([0-9])\1{2})');
+  static RegExp dobExp = new RegExp(r'^(0[1-9])|(1[0-2])/([1-9]|[12][0-9])|([3][01])/([2]([0-9])\1{3})|(19([0-9])\1{2})');
   
   static String validateName(String value, UserParameter<String> param, Function(UserParameter<String>) save){
     value = value.trim();
